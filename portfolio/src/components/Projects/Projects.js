ad712a1be44cfc15bb76eb5b98d61d295f656917
Meron Kebede <kebedemeron905@gmail.com>
2018-06-08 13:21:03 -0400

    project description added

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -13,27 +13,33 @@ class Projects extends Component {
             <h3>Bookshelf</h3>
             <p>A full-stack bookshelf app built using Express,
             Handlebars, and Mongoose.</p>
+            <a href='' class=''>View Code</a>
+            <a href='' class=''>Deployed App</a>
             <img src='/images/bookshelf.png' alt='bookshelf' />
+
           </div>
 
           <div className='project-two'>
             <h3>Seekr</h3>
-            <p>A full-stack bookshelf app built using Express,
-            Handlebars, and Mongoose.</p>
+            <p>A frontend React app that uses a Django REST framework API in the backend.<br />
+              Allows users to track their job application process. </p>
             <img src='/images/bookshelf.png' alt='bookshelf' />
+
           </div>
 
           <div className='project-three'>
             <h3>Salsa in the DMV</h3>
-            <p>A full-stack bookshelf app built using Express,
-            Handlebars, and Mongoose.</p>
+            <p>A full-stack Django app that allows users to track<br />
+               salsa events in the DMV.
+            </p>
             <img src='/images/bookshelf.png' alt='bookshelf' />
           </div>
 
           <div className='project-four'>
-            <h3>Seekr</h3>
-            <p>A full-stack bookshelf app built using Express,
-            Handlebars, and Mongoose.</p>
+            <h3>Flash Cards</h3>
+            <p>A simple JavaScript flash card game that asks<br />
+              users to identify titles of iconic artworks.
+            </p>
             <img src='/images/bookshelf.png' alt='bookshelf' />
           </div>
 
