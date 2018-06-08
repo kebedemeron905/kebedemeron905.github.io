8e89fac6db274c36e502309322e8208b87ef2bb5
Meron Kebede <kebedemeron905@gmail.com>
2018-06-07 20:28:55 -0400

    home page setup

---
+++ portfolio/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+class Home extends Component {
+  render () {
+    return (
+      <div className='loader'>
+        <h1 className='intro'>Meron Kebede</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
