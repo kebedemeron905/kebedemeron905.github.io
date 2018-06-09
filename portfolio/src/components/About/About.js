e0d83657ae3d1fa236b779f2b420ba8b395157d3
Meron Kebede <kebedemeron905@gmail.com>
2018-06-09 18:58:09 -0400

    scroll effect in place

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -5,7 +5,7 @@ class About extends Component {
   render () {
     return (
       <div id='about' className='about-me'>
-        <h1 className='about-h1'>About</h1>
+        <h1 className='about-h1'>About Me</h1>
         <img className='headshot' src='/images/meron.png' alt='headshot' />
       </div>
     )
--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -4,60 +4,62 @@ import './Projects.css'
 class Projects extends Component {
   render () {
     return (
-      <div id='projects' className='projects-container'>
+      <div id='projects'>
         <h1 className='projects-h1'>Projects</h1>
-        <div className='scrolling-wrapper'>
-          <div className='project-show'>
-            <h1 className='project-name'>Bookshelf</h1>
-            <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
-            <div className='icon-container'>
-              <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
-              <i className='devicon-mongodb-plain' />
-              <i className='devicon-express-original' />
-              <i className='devicon-nodejs-plain' />
+        <div className='projects-container'>
+          <div className='scrolling-wrapper'>
+            <div className='project-show'>
+              <h1 className='project-name'>Bookshelf</h1>
+              <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
+              <div className='icon-container'>
+                <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
+                <i className='devicon-mongodb-plain' />
+                <i className='devicon-express-original' />
+                <i className='devicon-nodejs-plain' />
+              </div>
+              <div className='project-link-container'>
+                <a className='project-link'>View Code</a>
+                <a className='project-link'>Deployed App</a>
+              </div>
+              <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
             </div>
-            <div className='project-link-container'>
-              <a className='project-link'>View Code</a>
-              <a className='project-link'>Deployed App</a>
-            </div>
-            <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
           </div>
-        </div>
-        <div className='scrolling-wrapper'>
-          <div className='project-show'>
-            <h1 className='project-name'>Seekr</h1>
-            <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
-            <div className='icon-container'>
-              <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
-              <i className='devicon-mongodb-plain' />
-              <i className='devicon-express-original' />
-              <i className='devicon-nodejs-plain' />
+          <div className='scrolling-wrapper'>
+            <div className='project-show'>
+              <h1 className='project-name'>Seekr</h1>
+              <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
+              <div className='icon-container'>
+                <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
+                <i className='devicon-mongodb-plain' />
+                <i className='devicon-express-original' />
+                <i className='devicon-nodejs-plain' />
+              </div>
+              <div className='project-link-container'>
+                <a className='project-link'>View Code</a>
+                <a className='project-link'>Deployed App</a>
+              </div>
+              <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
             </div>
-            <div className='project-link-container'>
-              <a className='project-link'>View Code</a>
-              <a className='project-link'>Deployed App</a>
-            </div>
-            <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
           </div>
-        </div>
-        <div className='scrolling-wrapper'>
-          <div className='project-show'>
-            <h1 className='project-name'>Famous Artworks</h1>
-            <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
-            <div className='icon-container'>
-              <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
-              <i className='devicon-mongodb-plain' />
-              <i className='devicon-express-original' />
-              <i className='devicon-nodejs-plain' />
+          <div className='scrolling-wrapper'>
+            <div className='project-show'>
+              <h1 className='project-name'>Famous Artworks</h1>
+              <p className='project-description'><span className='app'>App:</span> A full-stack bookshelf app built using <br />Express, handlebars, and Mongoose.</p>
+              <div className='icon-container'>
+                <h4 className='tools-used'>Tools Used: <span>MongDB, Express, Node</span></h4>
+                <i className='devicon-mongodb-plain' />
+                <i className='devicon-express-original' />
+                <i className='devicon-nodejs-plain' />
+              </div>
+              <div className='project-link-container'>
+                <a className='project-link'>View Code</a>
+                <a className='project-link'>Deployed App</a>
+              </div>
+              <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
             </div>
-            <div className='project-link-container'>
-              <a className='project-link'>View Code</a>
-              <a className='project-link'>Deployed App</a>
-            </div>
-            <img className='project-img'src='/images/bookshelf.png' alt='bookshelf' />
           </div>
-        </div>
 
+        </div>
       </div>
 
     )
