9320bb7ec59e8d9a183030eae6b8b5d8d3425115
Meron Kebede <kebedemeron905@gmail.com>
2018-06-09 20:17:27 -0400

    about me page

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -7,6 +7,14 @@ class About extends Component {
       <div id='about' className='about-me'>
         <h1 className='about-h1'>About Me</h1>
         <img className='headshot' src='/images/meron.png' alt='headshot' />
+        <div className='about-pp'>
+          <p className='about-line'> I love using code to tell stories and give organizations visibility. It’s powerful and exhilarating. I'm equally ecstatic about using my technical writing background to contribute to writing documentations for
+          emerging technologies.</p>
+          <p className='about-line'>My favorite stack right now is React on the frontend and Django on the backend!</p>
+          <p className='about-line'>Outside the browser, you'll most likely find me at a Cuban salsa event, reading contemporary fiction, or helping local high
+                          school students with their college application process.
+          </p>
+        </div>
       </div>
     )
   }
