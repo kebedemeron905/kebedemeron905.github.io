99553d166d76c029072ea2e3ff589ddcdda19069
Meron Kebede <kebedemeron905@gmail.com>
2018-06-09 22:11:38 -0400

    about component progress

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -4,18 +4,24 @@ import './About.css'
 class About extends Component {
   render () {
     return (
-      <div id='about' className='about-me'>
+      <div id='about'>
         <h1 className='about-h1'>About Me</h1>
-        <img className='headshot' src='/images/meron.png' alt='headshot' />
-        <div className='about-pp'>
-          <p className='about-line'> I love using code to tell stories and give organizations visibility. It’s powerful and exhilarating. I'm equally ecstatic about using my technical writing background to contribute to writing documentations for
-          emerging technologies.</p>
-          <p className='about-line'>My favorite stack right now is React on the frontend and Django on the backend!</p>
-          <p className='about-line'>Outside the browser, you'll most likely find me at a Cuban salsa event, reading contemporary fiction, or helping local high
+        <div className='about-me'>
+          <img className='headshot' src='/images/meron.png' alt='headshot' />
+          <div className='about-pp'>
+            <p className='about-line'> I love using code to give people the platform to tell their stories. It’s powerful and exhilarating. I'm equally ecstatic about using my technical writing background to contribute to writing documentations for
+              emerging technologies.</p>
+            <p className='about-line'>My favorite stack right now is React on the frontend and Django on the backend!</p>
+            <p className='about-line'>Outside the browser, you'll most likely find me at a Cuban salsa event, reading contemporary fiction, or helping local high
                           school students with their college application process.
-          </p>
+            </p>
+            <div className='skills-continer'>
+              <h1 className='skills-h1'>Skills:</h1>
+            </div>
+          </div>
         </div>
       </div>
+
     )
   }
 }
